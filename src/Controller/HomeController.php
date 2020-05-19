94d905fd50af7d49a567d9f9a4e707963f528943
Spartiate181 <kderkaoui00@gmail.com>
2020-05-19 13:33:32 +0200

    affichage recette

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="accueil")
      */
-    public function index()
+    public function index(Recipe $recipe)
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('recipes/index.html.twig', [
+            'recipe' => $recipe
         ]);
     }
 }
